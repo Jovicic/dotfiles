242d27943a1e63bf7487b6185da8f3878ceb0ba8
Nebojsa Jovicic <nebojsa@jovicic.at>
2024-05-14 01:38:09 +0200

    custom statusline (file name with relative path)

--- nvim/.config/nvim/lua/plugins/override.lua
+++ nvim/.config/nvim/lua/plugins/override.lua
@@ -9,6 +9,44 @@ return {
       -- buffer_leader_key = 'm', -- Per Buffer Mappings
     },
   },
+
+  -- custom statusline
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require "astroui.status"
+      local fileNameBlock = require "plugins.custom.filename"
+
+      opts.statusline = {
+        -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode {
+          mode_text = { padding = { left = 1, right = 1 } },
+        }, -- add the mode text
+        status.component.builder {
+          { provider = function() return vim.bo.fenc end },
+          surround = {
+            separator = "left",
+            condition = function() return vim.bo.fenc ~= "utf-8" end,
+          },
+          hl = { fg = "orange" },
+          update = "BufModifiedSet",
+        },
+        status.component.git_branch(),
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        fileNameBlock,
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.virtual_env(),
+        status.component.treesitter(),
+        status.component.nav(),
+        status.component.mode { surround = { separator = "right" } },
+      }
+    end,
+  },
   -- {
   --   "nvim-neo-tree/neo-tree.nvim",
   --   opts = function(_, opts)
