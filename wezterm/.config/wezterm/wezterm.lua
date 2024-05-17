2442c945213a719975c1e5600f7e982757c24ce7
Nebojsa Jovicic <nebojsa@jovicic.at>
2024-05-18 00:20:57 +0200

    use the new pwsh on windows

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,7 +19,7 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "powershell.exe" }
+	config.default_prog = { "pwsh.exe", "-NoLogo" }
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 	config.default_prog = { "/usr/bin/zsh" }
 end
