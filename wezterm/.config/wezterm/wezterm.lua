f97eb85638988951fcd599de3147c35a8c73c753
Nebojša Jovičić <nebojsa@jovicic.at>
2024-08-03 17:56:24 +0200

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -26,7 +26,8 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "pwsh.exe", "-NoLogo" }
+	-- config.default_prog = { "pwsh.exe", "-NoLogo" }
+	config.default_domain = 'WSL:Ubuntu-24.04'
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 	config.default_prog = { "/usr/bin/zsh" }
 end
